444fefdb2ff54ef700cb7ed4f2272e34202d9218
Francine Reinald Jaspeo <reinaldjaspeo.0205@gmail.com>
2024-05-21 15:33:57 +0800

    Made the index.php website functioning

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-    // connect to database
-    $conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
-
-    // check connection
-    if(!$conn) {
-        echo 'Connection error: ' . mysqli_connect_error();
-    }
+    include('config/db_connect.php');
 
     // write query from all pizzas
     $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
@@ -37,25 +31,32 @@
     <div class="container">
         <div class="row">
 
-            <?php foreach($pizzas as $pizza){ ?>
+            <?php foreach($pizzas as $pizza): ?>
                 
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
+                        <img src="img/pizza.svg" alt="pizza">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                             <ul>
-                                <?php foreach(explode(',', $pizza['ingredients']) as $ing){ ?>
+                                <?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
                                     <li><?php echo htmlspecialchars($ing); ?></li>
-                                <?php } ?>    
+                                <?php endforeach ?>    
                             </ul>
                         </div>
                         <div class="card-action right-align">
-                            <a href="#" class="brand-text">more info</a>                    </a>
+                            <a class="brand-text" href="details.php?id=<?php echo $pizza['id']; ?>">more info</a>
                         </div>
                     </div>
                 </div>
 
-            <?php } ?>
+            <?php endforeach; ?>
+
+            <!-- <?php if(count($pizzas) >= 2): ?>
+                <p>there are 2 or more pizzas</p>
+            <?php else: ?>
+                <p>there are less than 3 pizzas</p>
+            <?php endif; ?> -->
 
         </div>
     </div>
--- template/header.php
+++ template/header.php
@@ -13,14 +13,21 @@
             margin: 20px auto;
             padding: 20px;
         }
+        .pizza{
+            width: 100px;
+            margin: 40px auto -30px;
+            display: block;
+            position: relative;
+            top: -30px;
+        }
     </style>
 </head>
     <body class="grey lighten-4">
         <nav class="white z-depth-0">
             <div class="container">
-                <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+                <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
                 <ul id="nav-mobile" class="right hide-on-small-and-down">
-                    <li><a href="#" class="btn brand z-depth-0">Add A Pizza</a>
+                    <li><a href="add.php" class="btn brand z-depth-0">Add A Pizza</a>
                     </li>
                 </ul>
             </div>
